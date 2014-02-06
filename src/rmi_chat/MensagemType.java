4c5242398da3fd41f560e53e66dd911e39535f9b
nersoh <nelsonhenrique2611@gmail.com>
2014-02-06 11:25:29 -0300

    changed project folder name

---
+++ src/rmi_chat/MensagemType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rmi_chat;
+
+/**
+ *
+ * @author Nelson Henrique
+ */
+public enum MensagemType {
+    LOGIN, LOGOUT, PUBLIC, PRIVATE;
+}
